249ded247c7aef43e0c7525a317e13f6ed8d9067
Maksym Sobolyev <sobomax@sippysoft.com>
2020-06-10 18:37:27 -0700

    Allow any parameter to be overriden.

--- freebsd/resources/config.sh
+++ freebsd/resources/config.sh
@@ -1,32 +1,32 @@
 
 # FusionPBX Settings
-domain_name=ip_address          # hostname, ip_address or a custom value
-system_username=admin           # default username admin
-system_password=random          # random or as a pre-set value
-system_branch=master            # master, stable
+domain_name=${domain_name:-ip_address}         # hostname, ip_address or a custom value
+system_username=${system_username:-admin}      # default username admin
+system_password=${system_password:-random}     # random or as a pre-set value
+system_branch=${system_branch:-master}         # master, stable
 
 # FreeSWITCH Settings
-switch_enabled=true             # true or false
-switch_branch=stable            # master, stable (master requires source)
-switch_source=package           # package, port, source
-switch_tls=true                 # true or false
+switch_enabled=${switch_enabled:-true}         # true or false
+switch_branch=${switch_branch:-stable}         # master, stable (master requires source)
+switch_source=${switch_source:-package}        # package, port, source
+switch_tls=${switch_tls:-true}                 # true or false
 
 # Database Settings
-database_enabled=true           # true or false
-database_password=random        # random or as a pre-set value
-database_version=11             # Postgres 10, 9.6, 9.5, 9.4
-database_host=127.0.0.1         # hostname or IP address
-database_port=5432              # port number
-database_backup=false           # true or false
+database_enabled=${database_enabled:-true}     # true or false
+database_password=${database_password:-random} # random or as a pre-set value
+database_version=${database_version:-11}       # Postgres 10, 9.6, 9.5, 9.4
+database_host=${database_host:-127.0.0.1}      # hostname or IP address
+database_port=${database_port:-5432}           # port number
+database_backup=${database_backup:-false}      # true or false
 
 # Firewall Settings
-firewall_enabled=true           # true or false
+firewall_enabled=${firewall_enabled:-true}     # true or false
 
 # General Settings
-interface_name=auto             # auto, em0, igb0, vtnet0, or other valid names
-php_version=7.3                 # PHP version 7.2, 7.1, 5
-portsnap_enabled=true           # true or false
-sngrep_enabled=true             # true or false
-fail2ban_enabled=true           # true or false
-nginx_enabled=true              # true or false
-monit_enabled=false             # true or false
+interface_name=${interface_name:-auto}         # auto, em0, igb0, vtnet0, or other valid names
+php_version=${php_version:-7.3}                # PHP version 7.2, 7.1, 5
+portsnap_enabled=${portsnap_enabled:-true}     # true or false
+sngrep_enabled=${sngrep_enabled:-true}         # true or false
+fail2ban_enabled=${fail2ban_enabled:-true}     # true or false
+nginx_enabled=${nginx_enabled:-true}           # true or false
+monit_enabled=${monit_enabled:-false}          # true or false
